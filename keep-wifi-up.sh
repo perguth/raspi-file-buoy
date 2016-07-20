e4bc2cc1288d02bb44346c5b86c6f99f452216dd
Per Guth <mail@perguth.de>
2016-07-20 11:41:36 +0200

    keep-wifi-up.sh needs to be run via sudo

--- keep-wifi-up.sh
+++ keep-wifi-up.sh
@@ -2,7 +2,7 @@
 
 # place this file under `/root/keep-wifi-up.sh`
 # make executable: `chmod +x /root/keep-wifi-up.sh`
-# open crontab `crontab -e` and add `* * * * * bash /root/keep-wifi-up.sh`
+# open crontab `crontab -e` and add `* * * * * /usr/bin/sudo /root/keep-wifi-up.sh`
 
 if ifconfig wlan0 | grep -q "inet addr:" ; then
   exit 1
